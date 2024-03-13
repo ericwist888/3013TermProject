4532e1dde8e991cb609963e9bfd7dcb740a00d2e
ericwist888 <wistller@hotmail.com>
2024-03-13 13:53:08 -0700

    fifth commit

--- frontend/src/pages/Landing/Landing.page.jsx
+++ frontend/src/pages/Landing/Landing.page.jsx
@@ -11,8 +11,8 @@ const Landing = () => {
       </p>
       <div>
       <Turtle width={510} height={450} />
-      <img width ="410" height ="410" src="https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="niagra image" /><br /> 
-      <hr style={{height: '5px', backgroundColor: 'gray' }}/>
+      <img width ="70%" src="https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="niagra image" /><br /> 
+      {/* <hr style={{height: '5px', backgroundColor: 'gray' }}/> */}
       </div>
     </Container>
   );
